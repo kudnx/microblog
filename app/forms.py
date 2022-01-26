c7632f6e1cc4b796c735f630da2a8c47b2d5713a
kudnx <zluan009@gmail.com>
2022-01-25 23:38:46 -0300

    Web Forms

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Lembre-me')
+    submit = SubmitField('Entrar')
