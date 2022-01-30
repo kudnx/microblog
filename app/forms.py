02fbe5678b7e19926c5f8f44021ba51ba2f6e665
kudnx <zluan009@gmail.com>
2022-01-30 17:11:00 -0300

    Error Handling

--- app/forms.py
+++ app/forms.py
@@ -34,3 +34,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
     about_me = TextAreaField('Sobre mim', validators=[Length(min=0, max=140)])
     submit = SubmitField('Salvar')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Por favor escolha um outro nome de usuário.')
