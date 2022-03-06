b06719407f4b53f16fdc024fe508826f13d74d79
kudnx <zluan009@gmail.com>
2022-03-06 13:50:37 -0300

    Correção de bugs!

--- app/main/routes.py
+++ app/main/routes.py
@@ -72,7 +72,7 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('As suas modificações foram salvas.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
